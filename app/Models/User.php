474d99ecad415ba79917b7ab3fc7b64db61e30ff
engkuskusnadi <engkuskusnadi90@gmail.com>
2025-02-06 11:13:41 +0700

    menambahkan laravel ui auth dan bootsrap

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'nama_lengkap',
         'email',
         'password',
+        'role',
     ];
 
     /**
